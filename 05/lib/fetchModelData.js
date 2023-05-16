3468318f2a283f81617f53d8cb390c5a8a3b6add
acglontz <114431262+acglontz@users.noreply.github.com>
2023-05-15 23:40:15 -0400

    Create fetchModelData.js

---
+++ 05/lib/fetchModelData.js
@@ -0,0 +1,35 @@
+var Promise = require("Promise");
+
+/**
+ * FetchModel - Fetch a model from the web server.
+ *     url - string - The URL to issue the GET request.
+ * Returns: a Promise that should be filled
+ * with the response of the GET request parsed
+ * as a JSON object and returned in the property
+ * named "data" of an object.
+ * If the requests has an error the promise should be
+ * rejected with an object contain the properties:
+ *    status:  The HTTP response status
+ *    statusText:  The statusText from the xhr request
+ *
+ */
+
+function FetchModel(url) {
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => ({ data }))
+        .catch(error => {
+            throw new Error(error.message);
+        });
+}
+
+export default FetchModel;
+
+
+
+
